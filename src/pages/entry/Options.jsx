34282dfb02eb6cfd9c9483c19b85c18bf0e52be6
Sudharsan RaviKumar <supersudh@gmail.com>
2022-12-29 18:05:23 +0530

    Add Context API and wrap all tests with context

--- src/pages/entry/Options.jsx
+++ src/pages/entry/Options.jsx
@@ -5,11 +5,15 @@ import Row from 'react-bootstrap/Row';
 import ScoopOption from './ScoopOption';
 import ToppingOption from './ToppingOption';
 import AlertBanner from '../common/AlertBanner';
+import { pricePerItem } from '../../constants';
+import { formatCurrency } from '../../utilities';
+import { useOrderDetails } from '../../contexts/OrderDetails';
 
 export default function Options({ optionType }) {
 
   const [items, setItems] = useState([]);
   const [error, setError] = useState(false);
+  const { totals } = useOrderDetails();
 
   // optionType is 'scoops' or 'toppings'
   useEffect(() => {
@@ -18,8 +22,6 @@ export default function Options({ optionType }) {
         const { data } = await axios.get(`http://localhost:3030/${optionType}`);
         setItems(data);
       } catch (error) {
-        // console.log('Error in fetchScoopOrToppings');
-        // console.log(error);
         setError(true);
       }
     }
@@ -28,26 +30,29 @@ export default function Options({ optionType }) {
 
   if (error) {
     return (
-      <AlertBanner
-        message=""
-        variant=""
-      />
+      <AlertBanner />
     );
   }
 
   const ItemComponent = optionType === 'scoops' ? ScoopOption : ToppingOption;
+  const title = optionType[0].toUpperCase() + optionType.slice(1).toLowerCase();
 
-  const optionItems = items.map((item, i) => (
+  const optionItems = items.map((item) => (
     <ItemComponent
-      key={`item-${i + 1}`}
+      key={item.name}
       name={item.name}
       imagePath={item.imagePath}
     />
   ));
 
   return (
-    <Row>
-      {optionItems}
-    </Row>
+    <>
+      <h2>{title}</h2>
+      <p>{formatCurrency(pricePerItem[optionType])} each</p>
+      <p>{title} total: {formatCurrency(totals[optionType])}</p>
+      <Row>
+        {optionItems}
+      </Row>
+    </>
   );
 }
